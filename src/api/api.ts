45c91a9f2c548caccb4c66fb2d4151497ff7ed75
Алексей Беляев <alexeybelyaev@MacBook-Air-Aleksej.local>
2022-07-20 20:08:38 +0200

    proxy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const getFilms = (page: number) => {
-    return axios.get(`https://yts.torrentbay.to/api/v2/list_movies.json?limit=10&page=${page}`).then(res => {
+    return axios.get(`/api/v2/list_movies.json?limit=10&page=${page}`).then(res => {
         return res.data.data.movies
     })
 }
 
 export const searchFilms = (name: string) => {
-    return axios.get(`https://yts.torrentbay.to/api/v2/list_movies.json?query_term=${name}`).then(res => {
+    return axios.get(`/api/v2/list_movies.json?query_term=${name}`).then(res => {
         return res.data.data.movies
     })
 }
